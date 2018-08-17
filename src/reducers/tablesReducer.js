6c710900c9c1d7a1db83260890b8da39c48f92e2
Irakli <ikakochua@gmail.com>
2018-08-17 18:57:01 +0400

    Change

--- src/reducers/tablesReducer.js
+++ src/reducers/tablesReducer.js
@@ -1,10 +1,10 @@
-import { FECHT_TABLES } from "../actions/types";
+import { FECHT_TABLES } from "../actions/types"
 
 export default function(state = {}, action) {
   switch (action.type) {
     case FECHT_TABLES:
-      return action.payload;
+      return action.payload
     default:
-      return state;
+      return state
   }
 }
